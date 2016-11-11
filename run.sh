25924e76afd1d10d338a49e13152ea02e67e45d9
jgriffi6 <jgriffi6@gmail.com>
2016-11-10 20:45:53 -0600

    up

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ `head -c4 paymo_input/batch_payment.txt` == "time" ]; then
+    echo "doing simple test"
+    echo "usage is"
+    python src/antifraud.py -h
+    python src/antifraud.py -i paymo_input/batch_payment.txt -s paymo_input/stream_payment.txt
+    exit
+fi
+
 if [ ! -f paymo_input/batch_payment.csv ]; then
     `cat paymo_input/batch_payment.txt`
 fi
@@ -13,5 +21,6 @@ python src/antifraud.py -h
 
 echo ""
 echo 'Running src/antifraud.py'
+
 
 time python src/antifraud.py $@
